04b2441d04a458d258171bfa61e32dea83357f6d
cfdtirej <fkueee5113@gmail.com>
2020-11-26 13:28:21 +0900

    fix

---
+++ client.py
@@ -0,0 +1,38 @@
+import requests
+sample_json = {
+  "CarID" : 1,
+  "DateTime" : '2020/10/1 10:15:20.223',
+  "Position" : {
+    "GNSS-RTK" : {
+      "Latitude_deg" : 36.1234,
+      "Longitute_deg" : 137.332,
+      "Quality" : 1
+    },
+    "WiFi-RTT" : [
+      {
+        "PoleID" : 1,
+        "Distance_m" : 25.333
+      },
+      {
+        "PoleID" : 2,
+        "Distance_m" : 5.322
+      },
+      {
+        "PoleID" : 3,
+        "Distance_m" : 15.324
+      }
+    ]
+  },
+  "Azimuth_deg" : -30,
+  "Speed_level" : 5,
+  "Steering_level" : 3,
+  "ObstacleDistance_m" : 2.432
+}
+
+def send(url, data):
+    res = requests.post(url, json=data)
+    print(res.json())
+
+if __name__ == '__main__':
+    url = 'http://127.0.0.1:13000/'
+    send(url, sample_json)
--- src/server/app.py
+++ src/server/app.py
@@ -1,5 +1,4 @@
 import os
-from influxdb import line_protocol
 
 import yaml
 from flask import Flask, jsonify, request
@@ -10,22 +9,21 @@ from utils import IoTPoleDBClient
 config_yaml = os.path.join(os.path.dirname(__file__), 'config.yaml')
 with open(config_yaml, 'r') as f:
     conf = yaml.safe_load(f)
+    client = IoTPoleDBClient(
+        host=conf['InfluxDB']['host'],
+        port=conf['InfluxDB']['port'],
+        username=conf['InfluxDB']['username'],
+        password=conf['InfluxDB']['password'],
+        database=conf['InfluxDB']['database']
+    )
 
 app = Flask(__name__)
-
-client = IoTPoleDBClient(
-    host=conf['InfluxDB']['host'],
-    port=conf['InfluxDB']['port'],
-    username=conf['InfluxDB']['username'],
-    password=conf['InfluxDB']['password'],
-    database=conf['InfluxDB']['database']
-)
 
 
 @app.route('/', methods=['POST'])
 def home():
     req = request.get_json()
-    line_protocol = IoTPoleDBClient.req_json_to_line_plotocol(req)
+    line_protocol = client.req_json_to_line_plotocol(req)
     client.write_points(line_protocol)
     return jsonify(line_protocol)
 
