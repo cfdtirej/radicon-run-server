430fa343d32f4e1a5a6758a8ef348fd9a1ce6db5
cfdtirej <fkueee5113@gmail.com>
2020-12-03 19:16:07 +0900

    fix

---
+++ db_ctrl.py
@@ -0,0 +1,16 @@
+from influxdb import InfluxDBClient, client
+
+ql = '''
+SELECT * FROM "mobile"
+'''
+
+client = InfluxDBClient(
+    host='127.0.0.1',
+    port=18086,
+    username='root',
+    password='root',
+    database='IoT_Pole',
+)
+
+data = client.query(ql)
+print(data)
