3cb7bf58e9ec0c073bd8be723975a9d3269a3669
cfdtirej <fkueee5113@gmail.com>
2020-12-14 14:32:40 +0900

    fix

--- server/src/utils.py
+++ server/src/utils.py
@@ -1,6 +1,9 @@
+import json
+import os
 from datetime import date, datetime
-from typing import Dict
+from typing import List, Dict, Union
 
+from scipy.optimize import minimize
 import numpy as np
 from influxdb import InfluxDBClient
 
@@ -11,8 +14,8 @@ def calc_xy(phi_deg, lambda_deg, phi0_deg=36.0, lambda0_deg=137.0 + 10/60) -> Di
     lambda_rad = np.deg2rad(lambda_deg)
     phi0_rad = np.deg2rad(phi0_deg)
     lambda0_rad = np.deg2rad(lambda0_deg)
+    
     # 補助関数
-
     def A_array(n):
         A0 = 1 + (n**2)/4. + (n**4)/64.
         A1 = -     (3./2)*(n - (n**3)/8. - (n**5)/64.)
@@ -63,8 +66,35 @@ def calc_xy(phi_deg, lambda_deg, phi0_deg=36.0, lambda0_deg=137.0 + 10/60) -> Di
                                                     np.sinh(2*eta2*np.arange(1, 6))))))  # [m]
     return {'x': x, 'y': y}
 
+def gps_solve(distances_to_station, stations_coordinates):
+    def error(x, c, r):
+        return sum([(np.linalg.norm(x - c[i]) - r[i]) ** 2 for i in range(len(c))])
+    l = len(stations_coordinates)
+    S = sum(distances_to_station)
+    # compute weight vector for initial guess
+    W = [((l - 1) * S) / (S - w) for w in distances_to_station]
+    # get initial guess of point location
+    x0 = sum([W[i] * stations_coordinates[i] for i in range(l)])
 
-def poleID_dist_m(data: list) -> dict:
+    # optimize distance from signal origin to border of spheres
+    return minimize(error, x0, args=(stations_coordinates, distances_to_station), method='Nelder-Mead').x
+
+APPOS_JSON = os.path.join(os.path.dirname(__file__), 'APPOS.json')
+def multilateration(wifi_rtt: List[Dict[str, Union[int, float]]]) -> Dict[str, float]:
+	distances_to_station = []
+	for idx in range(1, 4):
+		for pole_info in wifi_rtt:
+			if pole_info['PoleID'] == idx:
+				distances_to_station.append(pole_info['Distance_m'])
+	with open(APPOS_JSON, 'r') as f:
+		appos = json.load(f)['AP Pos']
+		appos_lists: List[List[float, float]]  = [ap['Position '] for ap in appos]
+		stations: List[np.ndarray[np.float, np.float]] = list(np.array(appos_lists))
+
+	coordinate = gps_solve(distances_to_station, stations)
+	return {'x': coordinate[0], 'y': coordinate[1]}
+
+def poleID_dist_m(data: List[Dict[str, Union[int, float]]]) -> Dict[str, float]:
     '''
     data: request_json['Position']['WiFi-RTT']
     [
@@ -88,10 +118,37 @@ def to_rfc3339(dt):
     except ValueError:
         dt_3339 = datetime.strptime(dt+'+0900', '%Y-%m-%d %H:%M:%S.%f%z').isoformat()
         return dt_3339
+
+
 class IoTPoleDBClient(InfluxDBClient):
 
     @classmethod
     def req_json_to_line_plotocol(cls, request_json: dict) -> list:
+        GNSS_RTK_xy = calc_xy(request_json['Position']['GNSS-RTK']['Latitude_deg'],
+                              request_json['Position']['GNSS-RTK']['Longitute_deg'])
+        WiFi_RTT_xy = multilateration(request_json['Position']['WiFi-RTT'])
+        line_protocol = [{
+            'time': to_rfc3339(request_json['DateTime']),
+            'measurement': 'mobile',
+            'tag': {
+                'CarID': request_json['CarID']
+            },
+            'fields': {
+                'GRSS_RTK_x': GNSS_RTK_xy['x'],
+                'GRSS_RTK_y': GNSS_RTK_xy['y'],
+                'Quality': request_json['Position']['GNSS-RTK']['Quality'],
+                'WiFi_RTT_x': WiFi_RTT_xy['x'],
+                'WiFi_RTT_y': WiFi_RTT_xy['y'],
+                'Azimuth_deg': request_json['Azimuth_deg'],
+                'Speed_level': request_json['Speed_level'],
+                'Steering_level': request_json['Steering_level'],
+                'ObstacleDistance_m': request_json['ObstacleDistance_m']
+            }
+        }]
+        return line_protocol
+
+    @classmethod
+    def req_json_to_line_plotocol_old_version(cls, request_json: dict) -> list:
         GNSS_RTK_xy = calc_xy(request_json['Position']['GNSS-RTK']['Latitude_deg'],
                               request_json['Position']['GNSS-RTK']['Longitute_deg'])
         WiFi_RTT_dist = poleID_dist_m(request_json['Position']['WiFi-RTT'])
